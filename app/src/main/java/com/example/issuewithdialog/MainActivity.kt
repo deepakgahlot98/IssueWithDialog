c77604685754e0d9a6e4f864017f9c5ea9876c1e
Deepak Gahlot <depak.gahlot@sap.com>
2021-02-26 20:47:51 +0530

    Issue with registerforActivityResult
    updating the sample project

--- app/src/main/java/com/example/issuewithdialog/MainActivity.kt
+++ app/src/main/java/com/example/issuewithdialog/MainActivity.kt
@@ -2,23 +2,95 @@ package com.example.issuewithdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.registerForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionContext
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
-
-    val list = arrayListOf<String>("Option 1", "Option 2", "Option 3", "Other")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MultiSelectComponent(list)
+            AttachmentComponent()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AttachmentComponent() {
+
+    var fileName = remember { mutableStateOf(ArrayList<String>()) }
+    var array = arrayOf("application/pdf")
+    var files = ArrayList<String>()
+    var fileAttached = remember { mutableStateOf(false) }
+
+
+    Column {
+
+        val registerTakeFile = registerForActivityResult(
+            ActivityResultContracts.OpenMultipleDocuments()
+        ) {
+            for (i in 0 until it.size) {
+                var uri = it.get(i)
+                files.add(uri.path!!)
+            }
+            fileName.value = files
+        }
+
+        Text(
+            text = "Attachment",
+            color = colorResource(id = R.color.black)
+        )
+        if (fileAttached.value) {
+            FileAttached(item = fileName.value)
+        }
+        OutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            colors = ButtonDefaults.outlinedButtonColors(
+                contentColor = colorResource(id = R.color.black)
+            ),
+            onClick = {
+                registerTakeFile.launch(array)
+                fileAttached.value = true
+            }
+        ) {
+            Icon(painterResource(id = R.drawable.attach), contentDescription = "attach")
+            Text(
+                fontSize = 14.sp,
+                text = "Add Files",
+            )
         }
     }
 }
 
 @Composable
-fun MultiSelectComponent(list: ArrayList<String>) {
-    MultiSelectPickList(list)
+fun FileAttached(item: ArrayList<String>) {
+    Column {
+        item.forEach {
+            Row {
+                Text(
+                    text = it,
+                    color = colorResource(id = R.color.black)
+                )
+            }
+        }
+    }
 }
